65db50d5231172cf6ce5afd768780d2f18b50a67
chef27615 <wei.du.2015@marshall.usc.edu>
2019-08-22 17:50:16 -0400

    more bug need to found

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,14 +7,55 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        ram = [0] * 256
-        self.ram = ram
-        
-        reg = [0] * 8
-        self.reg = reg
+        # init ram
+        self.ram = [000000000] * 256
+        # init pointer
+        self.pc = 0
+        # init register
+        self.reg = [0] * 8 
+        # init stack_pointer, point from the end 
+        self.stack_pointer = 0xF4 
+        self.reg[7] = self.ram[self.stack_pointer]
+        #op dictionary 
+        self.op_table = {}
+        self.op_table[0b01000111] = self.op_prn
+        self.op_table[0b00000001] = self.op_hlt
+        self.op_table[0b10000010] = self.op_ldi
+        self.op_table[0b01000101] = self.op_push
+        self.op_table[0b01000110] = self.op_pop
+        self.op_table[0b00010001] = self.op_ret
+        self.op_table[0b01010000] = self.op_call
 
-        pc = 0
-        self.pc = pc
+    def op_prn(self, num):
+        print(self.reg[num])
+    
+    def op_hlt(self):
+        sys.exit(1)
+    
+    def op_ldi(self, reg_a, reg_b):
+        self.reg[reg_a] = reg_b
+    
+    def op_push(self, num):
+        val = self.reg[num]
+        self.stack_pointer -= 1
+        self.ram[self.stack_pointer] = val
+    
+    def op_pop(self, num):
+        val = self.ram[-1]
+        self.reg[num] = val
+        self.stack_pointer +=1
+
+    def op_ret(self):
+        val = self.ram[-1]
+        self.pc = val
+        self.stack_pointer +=1
+    
+    def op_call(self, num):
+        self.stack_pointer -= 1
+        address = self.pc + 2
+        self.ram[self.stack_pointer] = address
+        sub_route = self.reg[num]
+        self.pc = sub_route
 
     def load(self):
         """Load a program into memory."""
@@ -26,45 +67,21 @@ class CPU:
         try: 
             with open(sys.argv[1]) as f:
                 for line in f:
-                    comment_split = line.split('#')
-                    num = comment_split[0].strip()
+                    number = line.split('#')[0]
+                    number = number.replace('\n', '')
+                    number = number.strip()
 
-                    if num =='':
-                        continue
+                    if number =='':
+                        number= int(number, 2)
                     
-                    value = int(num, 2)
-                    print(f'value in loading {value}')
-                    self.ram[address] = value
-
+                    print(f'value in loading {number}')
+                    self.ram[address] = number
                     address += 1
         
         except FileNotFoundError:
             print(f'{sys.argv[0]}: sys.argv[1] not found')
             sys.exit()
 
-        # if len(sys.argv) != 2:
-        #     print('Usage: using file <filename>', file=sys.stderr)
-        #     sys.exit(1)
-
-
-        # address = 0
-
-        # # For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010, # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111, # PRN R0
-        #     0b00000000,
-        #     0b00000001, # HLT
-        # ]
-
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
-    
     def ram_read(self, address):
         return self.ram[address]
 
@@ -73,15 +90,34 @@ class CPU:
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
+        MUL = 0b10100010
+        ADD = 0b10100000
+        DEC = 0b10100001
+        DIV = 0b10100011
+        XOR = 0b10101011
+        SHR = 0b10101101
+        SHL = 0b10101100
 
-        if op == "ADD":
+        if op == ADD:
             self.reg[reg_a] += self.reg[reg_b]
-        elif op == "SUB":
+        elif op == DEC:
             self.reg[reg_a] -= self.reg[reg_b]
-        elif op == "MUL":
+        elif op == MUL:
             self.reg[reg_a] *= self.reg[reg_b]
-        elif op == "DIV":
+        elif op == DIV:
             self.reg[reg_a] /= self.reg[reg_b]
+        elif op == XOR:
+            xor = self.reg[reg_a] ^ self.reg[reg_b]
+            self.reg[reg_a] = xor
+        elif op == SHR:
+            shr = self.reg[reg_a]
+            right = shr >> self.reg[reg_b]
+            self.reg[reg_a] = right
+        elif op == SHL:
+            shl = self.reg[reg_a]
+            left = shl << self.reg[reg_b]
+            self.reg[reg_a] = left
+
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -107,25 +143,43 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        running = True
-        # print('______\n')
-        # self. trace()
-        # print('______\n')
+        
+        ir = self.ram[self.pc]
+        print(f'ir: {ir}')
 
-        while running:
-            cmd_list = self.ram
-            for cmd in cmd_list:
-                if cmd == 130:
-                    pass
-                elif cmd == 1:
-                    running = False
-                elif cmd == 0:
-                    self.pc += 1
-                elif cmd == 8:
-                    num = cmd_list[self.pc+1]
-                elif cmd == 71:
-                    print(num)
-                else:
-                    print(f'unknown cmd: {cmd}')
+        while True:
+            ir = self.ram[self.pc]
+
+            cmd_a = self.ram_read(self.pc+1)
+            cmd_b = self.ram_read(self.pc+2)
+            print(cmd_a, cmd_b)
+
+            cpu_op = (ir & 0b11000000) >> 6 
+            alu_op = (ir & 0b00100000) >> 5 
+            
+            if ir == 0b01010000:
+                self.op_table[ir](cmd_a)
+                continue
+
+            elif ir == 0b00010001:
+                self.op_table[ir]()
+                continue
+            
+            if alu_op:
+                self.alu(ir, cmd_a, cmd_b)
+            elif cpu_op == 2:
+                self.op_table[ir](cmd_a, cmd_b)
+            elif cpu_op == 1:
+                self.op_table[ir](cmd_a)
+            elif cpu_op == 0:
+                self.op_table[ir]()
+            else:
+                self.op_table[ir]()
+            self.pc += cpu_op + 1
+
+        print(f'ir: {ir}')
+        # cmd_a = (ir )
+
+            
         # print(f'ram {self.ram}')
         # print(f'pc {self.pc}')
